962e2fa26f6c5d5752fef022f4f42032d3f18439
hanshengzhang <hanshengzhang@pptv.com>
2016-11-01 11:40:51 +0800

    test link to github

---
+++ activity/src/main/java/cn/say/anything/conrtoller/HelloContoller.java
@@ -0,0 +1,10 @@
+package cn.say.anything.conrtoller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/hello")
+public class HelloContoller {
+
+}
